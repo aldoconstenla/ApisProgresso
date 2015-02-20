6f8d7a9bccb346465e07838ba3a68d5173674792
Aldo <aldo.constenla@gmail.com>
2015-02-20 00:05:40 -0200

    First commit

--- js/animation.js
+++ js/animation.js
@@ -1,13 +1,14 @@
 $( document ).ready(function() {
 
+	//Rolagem animada
 	$(function() {
 	    $('a').bind('click',function(event){
+	    	event.preventDefault();
 	        var $anchor = $(this);
 	        $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top}, 1000,'swing');
 			
 			// Outras Animações
 			// linear, swing, jswing, easeInQuad, easeInCubic, easeInQuart, easeInQuint, easeInSine, easeInExpo, easeInCirc, easeInElastic, easeInBack, easeInBounce, easeOutQuad, easeOutCubic, easeOutQuart, easeOutQuint, easeOutSine, easeOutExpo, easeOutCirc, easeOutElastic, easeOutBack, easeOutBounce, easeInOutQuad, easeInOutCubic, easeInOutQuart, easeInOutQuint, easeInOutSine, easeInOutExpo, easeInOutCirc, easeInOutElastic, easeInOutBack, easeInOutBounce
-	                  
 	    });
 	});
 
@@ -20,6 +21,17 @@ $( document ).ready(function() {
 	    }
 	});
 
+
+
+	//Plugin de Scrollbar
+	function scrollbar() {
+		"use strict";
+    	$('#Scrollbar').perfectScrollbar();
+	}
+
+
+
+    //Posicionamento de ícones de Produtos
 	var i = 1,
 		resize = true,
 		categoria = $('.product');
@@ -29,16 +41,138 @@ $( document ).ready(function() {
 		$(element).addClass('resized');
 		i++;
 		$(element).animate({
-			left: left,
+			marginLeft: left,
 			top: top
-		}, 200, function() {
+		}, 300, function() {
 			if (i > $('.product').length) {
 				$('.prod-display').addClass('active');
 			}
 		});
 	}
 
-	// function reposition2() { //Função com um ícone por vez
+	var checkCategory;
+
+	function alternaCategoria(categoria) {
+		$('.category').removeClass('active');
+		$('.category.'+categoria).addClass('active');
+	}
+
+	$('.product').on('click', function(e) {
+		e.preventDefault();
+		checkCategory = $(this).data('name');
+		if (resize) {
+
+			//Todos ícones juntos (função única):
+			for (var j = 0; j < categoria.length; j++) {
+				reposition(categoria.eq(j), categoria.eq(j).data('left'), categoria.eq(j).data('top'));
+			}
+
+			resize = false;
+			recebeProdutos(checkCategory);
+		}
+		$('.product').removeClass('active');
+		$(this).addClass('active');
+		alternaCategoria(checkCategory);
+	});
+
+
+
+
+	//Lightbox
+	var category, numItem, qtd, side;
+
+	function ativaLightbox(categoria,numitem) {
+		$('.prodLightbox').addClass('active');
+		$('.overlay').addClass('active');
+		$('.prodLightbox').find('.'+categoria+'.item'+numitem).addClass('active');
+		$('.arrows').addClass('active');
+	}
+
+	$(document).on('click', '.category a', function(event) {
+		event.preventDefault();
+		category = $(this).parent().parent().parent().data('categoria');
+		numItem = $(this).data('item');
+		recebeProdLightbox(category, numItem);
+		ativaLightbox(category, numItem);
+	});
+
+	$('.overlay').on('click', function() {
+		$('.prodLightbox').removeClass('active');
+		$('.overlay').removeClass('active');
+		$('.prodLightbox').find('.container').removeClass('active');
+		$('.arrows').removeClass('active');
+	});
+
+	$('.arrow').on('click', function() {
+		qtd = $('.category.'+category).find('a').length;
+		side = $(this).data('side');
+		$('.prodLightbox').find('.container').removeClass('active');
+		if((numItem < qtd && side == 'right') || (numItem > 1 && side == 'left')) {
+			if (side == 'right'){
+				numItem += 1;
+			} else {
+				numItem -= 1;
+			}
+		} else {
+			if (side == 'right'){
+				numItem = 1;
+			} else {
+				numItem = qtd;
+			}
+		}
+		$('.prodLightbox').find('.'+category+'.item'+numItem).addClass('active');
+	});
+
+
+
+
+	//AJAX que recebe itens dos produtos nas categorias
+	function recebeProdutos(categoria) {
+		$.ajax('lista-produtos.html', {
+			success: function(response){ $('.prod-display').html(response); },
+			complete: function() { alternaCategoria(categoria); scrollbar(); }
+		});
+	}
+
+	function recebeProdLightbox(category, numitem) {
+		$.ajax('prodcategoria-'+category+'.html', {
+			success: function(response){ $('.prodLightbox').html(response); },
+			complete: function() {
+				console.log('recebeProdLightbox ok');
+				ativaLightbox(category, numitem);
+			}
+		});
+	}
+
+});
+
+
+
+
+
+
+// $('.arrow.left').on('click', function() {
+// 	qtd = $('.category.'+category).find('a').length;
+// 	if(numItem > 1) {
+// 		// console.log(qtd);
+// 		$('.prodLightbox').find('.container').removeClass('active');
+// 		numItem -= 1;
+// 		$('.prodLightbox').find('.'+category+'.item'+numItem).addClass('active');
+// 	} else {
+// 		$('.prodLightbox').find('.container').removeClass('active');
+// 		numItem =  = qtd;;
+// 		$('.prodLightbox').find('.'+category+'.item'+numItem).addClass('active');
+// 	}
+// });
+
+
+// for (var j = 0; j < item.length; j++) {
+
+// }
+
+
+
+// function reposition2() { //Função com um ícone por vez
 	// 	$('.mel').addClass('resized');
 	// 	$('.mel').animate({
 	// 		left: '45px',
@@ -77,37 +211,14 @@ $( document ).ready(function() {
 	// } 
 
 
-	$('.product').on('click', function(e) {
-		e.preventDefault();
-		if (resize) {
-			
-			//Todos ícones juntos (função única):
-			for (var j = 1; j > categoria.length; j++) {
-				reposition(categoria.eq(j), categoria.eq(j).data('left'), categoria.eq(j).data('top'));
-			}
-			
-			// reposition('.mel', '45px', '150px');
-			// reposition('.propolis', '180px', '150px');
-			// reposition('.granola', '45px', '285px');
-			// reposition('.xarope', '180px', '285px');
-			// reposition('.geleia', '45px', '420px');
-			// reposition('.outros', '180px', '420px');
+	// reposition('.mel', '45px', '150px');
+	// reposition('.propolis', '180px', '150px');
+	// reposition('.granola', '45px', '285px');
+	// reposition('.xarope', '180px', '285px');
+	// reposition('.geleia', '45px', '420px');
+	// reposition('.outros', '180px', '420px');
 
-			//Função com um ícone por vez (função recursiva):
-			//reposition2();
+	//Função com um ícone por vez (função recursiva):
+	//reposition2();
 
-			// $('.prod-display').addClass('active');
-
-			resize = false;
-		}
-		$('.product').removeClass('active');
-		$(this).addClass('active');
-	});
-
-});
-
-
-//Produtos
-
-//left: 45px/180px
-//top: 150px/285px/420px
+	// $('.prod-display').addClass('active');
